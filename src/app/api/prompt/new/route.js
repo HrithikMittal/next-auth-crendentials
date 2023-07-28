104f27b51a0143609992e1a35a7ad9eb3cc7798d
Adhikansh Mittal <adhikanshmittal@Adhikanshs-MacBook-Pro-2.local>
2023-07-29 00:26:57 +0530

    add prompt to db

---
+++ src/app/api/prompt/new/route.js
@@ -0,0 +1,24 @@
+import { connectToDB } from "@/utils/database";
+import Prompt from "@/models/Prompt";
+
+export const POST = async (req, res) => {
+  const { userId, prompt, tag } = await req.json();
+  try {
+    await connectToDB();
+    console.log("re", req.body);
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), {
+      status: 201,
+    });
+  } catch (err) {
+    console.log(err);
+    return new Response(JSON.stringify(err), {
+      status: 500,
+    });
+  }
+};
